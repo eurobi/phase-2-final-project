38c02ed918ed172429b6e00e76727aeafb77d5ae
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-09-26 17:34:17 -0400

    rating shows properly on review card

--- my-app/src/Components/Review.js
+++ my-app/src/Components/Review.js
@@ -1,13 +1,21 @@
 function Review({ review }){
     function handleStars(rating){
-        
+        let stars = []
+        for(let i = 1; i <= 5; i ++){
+            if(i <= rating){
+                stars.push('★')
+            }else{
+                stars.push('☆')
+            }
+        }
+        return stars.join('')
     }
     return(
         <div className='card'>
             <h1>{review.title}</h1>
             <h3>{review.category}</h3>
             <p>{review.content}</p>
-            <p> ★ ★ ★ ☆ ☆ </p>
+            <p>{handleStars(review.rating)}</p>
         </div>
     )
 }
